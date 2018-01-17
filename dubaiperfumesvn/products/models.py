508494b2f7a216fc86123637bfac7b39d3cbc5c1
pvn-quyen-le <quyen.le@punch.vn>
2018-01-17 11:09:23 +0700

    first commit

--- dubaiperfumesvn/products/models.py
+++ dubaiperfumesvn/products/models.py
@@ -11,12 +11,12 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-class Product(models.Model):
 
+class Product(models.Model):
     code = models.CharField(max_length=100, unique=True)
     name = models.CharField(max_length=200)
     price = models.PositiveIntegerField()
-    description = models.CharField(max_length=800)
+    description = models.TextField()
     info = models.TextField()
     star = models.FloatField(default=4.0)
     category = models.ForeignKey('Category', on_delete=models.PROTECT,
@@ -24,3 +24,9 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
+class Landing(models.Model):
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
+    titile = models.CharField(max_length=100)
+    message = models.CharField(max_length=500)
+    redirect_link = models.URLField()
--- dubaiperfumesvn/products/views.py
+++ dubaiperfumesvn/products/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Product
 
 # Create your views here.
 def index(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products': products}
     return render(request, 'products/index.html', context)
